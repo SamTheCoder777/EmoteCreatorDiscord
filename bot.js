8a6e414c4f2e8d9de05896013d0fb423482701cd
samthecoder <49662629+SamTheCoder777@users.noreply.github.com>
2020-08-18 12:39:14 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,50 @@
+// server.js
+// where your node app starts
+
+// init project
+const http = require("http");
+const express = require("express");
+const app = express();
+const talkedRecently = new Set();
+const warned1 = new Set();
+const warned2 = new Set();
+const wasMuted = new Set();
+var status = "Helping World President";
+
+const config = require("./config.json");
+var opus = require("opusscript");
+const googleIt = require("google-it");
+
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const delay = msec => new Promise(resolve => setTimeout(resolve, msec));
+
+client.on("ready", () => {
+  console.log("I am Online\nI am Online");
+  client.user.setActivity("Sumi best girl", {
+    type: "PLAYING"
+  });
+});
+
+const prefix = "^";
+
+client.on("message", async message => {
+  const args = message.content
+    .slice(config.prefix.length)
+    .trim()
+    .split(/ +/g);
+  const command = args.shift().toLowerCase();
+  let messageArray = message.content.split(" ");
+  
+  });
+
+client.login(process.env.TOKEN);
