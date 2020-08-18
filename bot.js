f5f2391841164b0b8489ccb1a576f46aecb3e74f
samthecoder <49662629+SamTheCoder777@users.noreply.github.com>
2020-08-18 15:02:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,9 +47,9 @@ client.on("message", async message => {
   
  
  if (message.content.startsWith(prefix + "addEmote")){
-  let guild = '718585327161442304';
-  var image = "";
-  var name = "";
+  var guild = '718585327161442304';
+  var image = '';
+  var name = '';
   const filter = m => m.author.id === message.author.id;
   const collector = message.channel.createMessageCollector(filter, {
   time: 30000, errors: ["time"], max: 1 
